6a9836d8da3a1f5357bb5843e452346aaabd35b9
Alex <aikluger@gmail.com>
2019-01-23 14:52:56 -0500

    added last of command functions and moment

--- liri.js
+++ liri.js
@@ -4,12 +4,52 @@ var keys = require("./keys.js");
 var fs = require('fs');
 var axios = require("axios");
 var Spotify = require("node-spotify-api");
+var moment = require("moment");
 // console.log(process.argv[2])
+
+
+function concertThis(artist)  {
+
+var artist = process.argv.slice(3).join(" ");
+
+if (!artist)    {
+    artist = "Action Bronson"
+    // what to do here..
+}
+
+
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+    axios.get(queryUrl).then(
+        function (response) {
+// fix THIS
+for (var i = 0; i < response.data.length; i++)
+
+            if (artist) {
+            var concertInfo = response.data[i];
+            var dateFormat = moment.ISO_8601;
+            var concertDate = concertInfo.datetime
+            var convertedDate = moment(concertDate, dateFormat);
+        console.log( "\n" + "Venue Name: " + concertInfo.venue.name + "\n" +
+           "Location " + concertInfo.venue.city + "\n" +
+           "Date: " + convertedDate.format("MM/DD/YY"))
+            }
+})
+}
+// user commands:
+// concert-this artist name
+// Name of the venue
+// Venue location
+// Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+
+
+var songName = process.argv.slice(3).join(" ");
 
 function getSong(songName) {
 
     var spotify = new Spotify(keys.spotify);
-    var songName = process.argv.slice(3).join(" ");
+    
     //put this outside the function...
 
     if (!songName) {
@@ -28,11 +68,6 @@ function getSong(songName) {
 
     })
 }
-// user commands:
-// concert-this artist name
-// Name of the venue
-// Venue location
-// Date of the Event (use moment to format this as "MM/DD/YYYY")
 
 
 // spotify-this-song song name
@@ -40,10 +75,11 @@ function getSong(songName) {
 // The song's name
 // A preview link of the song from Spotify
 // The album that the song is from
+var movieName = process.argv.slice(3).join(" ");
 
 var getMovie = function (movieName) {
 
-    var movieName = process.argv.slice(3).join(" ");
+    
 
     if (!movieName) {
         movieName = "Mr Nobody";
@@ -69,7 +105,30 @@ var getMovie = function (movieName) {
         })
     }
 
-//if user does not enter argument return "Mr. Nobody" search
-// do-what-it-says use .fs to grab instructions from random.txt file;
+
+function doWhatItSays() {
+    // grab instructions from random.txt file
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+        if (error) {
+            return console.log(error);
+          }
+
+          var dataArr = data.split(",");
+
+          for (var i =0; i < dataArr.length; i++)
+          if (dataArr[i]==="spotify-this-song") {
+                getSong(dataArr[i+1])
+          }
+  // We will then re-display the content as an array for later use.
+  console.log(dataArr);
+    })
+}
+
+
+
 // getSong()
-getMovie();
+// getMovie();
+// doWhatItSays();
+concertThis();
+
