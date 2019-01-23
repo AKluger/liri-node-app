2b1438dc3bf54f2c989879e41ffd572bd1eeee47
Alex <aikluger@gmail.com>
2019-01-22 22:44:57 -0500

    added imdb functionality and began bands code

--- liri.js
+++ liri.js
@@ -2,22 +2,32 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var fs = require('fs');
+var axios = require("axios");
 var Spotify = require("node-spotify-api");
-console.log(process.argv[2])
+// console.log(process.argv[2])
 
-// function getSong(songName)  {
-var spotify = new Spotify(keys.spotify);
-var songName = process.argv[2];
+function getSong(songName) {
 
-spotify.search({ type: 'track', query: songName }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+    var spotify = new Spotify(keys.spotify);
+    var songName = process.argv.slice(3).join(" ");
+    //put this outside the function...
+
+    if (!songName) {
+        songName = "The Sign"
     }
-   
-  console.log(data); 
 
-})
-// }
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log("\n" + "Artist: " + data.tracks.items[0].album.artists[0].name + "\n" +
+            "Title: " + songName + "\n" +
+            "Listen here: " + data.tracks.items[0].album.external_urls.spotify + "\n" + 
+            "Album: " + data.tracks.items[0].album.name);
+
+    })
+}
 // user commands:
 // concert-this artist name
 // Name of the venue
@@ -31,17 +41,35 @@ spotify.search({ type: 'track', query: songName }, function(err, data) {
 // A preview link of the song from Spotify
 // The album that the song is from
 
-// return Ace of Base The Sign if argument is blank
+var getMovie = function (movieName) {
 
-// movie-this movie name
-// * Title of the movie.
-// * Year the movie came out.
-// * IMDB Rating of the movie.
-// * Rotten Tomatoes Rating of the movie.
-// * Country where the movie was produced.
-// * Language of the movie.
-// * Plot of the movie.
-// * Actors in the movie.
+    var movieName = process.argv.slice(3).join(" ");
+
+    if (!movieName) {
+        movieName = "Mr Nobody";
+    }
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
+
+    axios.get(queryUrl).then(
+        function (response) {
+
+        console.log( "\n" + "Title: " + response.data.Title + "\n" +
+           "Year: " + response.data.Year + "\n" +
+           "IMDB Rating: " + response.data.imdbRating + "\n" +
+           "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n" +
+           "Country of Production: " + response.data.Country + "\n" +
+           "Language: " + response.data.Language + "\n" +
+           "Plot: " + response.data.Plot + "\n" +
+           "Cast: " + response.data.Actors
+           )
+        })
+    }
 
 //if user does not enter argument return "Mr. Nobody" search
 // do-what-it-says use .fs to grab instructions from random.txt file;
+// getSong()
+getMovie();
